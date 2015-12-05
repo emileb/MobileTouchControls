faa6089b33b5277d2d1ba7e2223eb66713e5186f
Emile <emile@Emiles-Mac.local>
2015-12-05 12:34:42 +0000

    Adding IOS support

---
+++ GLheader.h
@@ -0,0 +1,13 @@
+//
+//  GLheader.h
+//  touchcontrols
+//
+//  Created by Emile on 05/12/2015.
+//  Copyright © 2015 Beloko games. All rights reserved.
+//
+
+#ifndef GLheader_h
+#define GLheader_h
+
+
+#endif /* GLheader_h */
